9d0ac28264bb92b7bfed4fbc19e0790a91637ce3
harigaurav <163562634+harigaurav@users.noreply.github.com>
2024-12-23 13:24:12 +0530

    Create 08.cpp

---
+++ 08.cpp
@@ -0,0 +1,5 @@
+//Q-Given a Doubly Linked list and a position. The task is to delete a node from a given position
+//(position starts from 1) in a doubly linked list and return the head of the doubly Linked list.
+
+
+//SOLUTION-
